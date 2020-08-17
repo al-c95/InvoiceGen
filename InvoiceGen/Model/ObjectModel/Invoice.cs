3f2a8d6b4e03fe847200bc8744e3f584e00d71ec
Alc2110 <38523363+Alc2110@users.noreply.github.com>
2020-08-17 21:47:36 +1000

    implement and test object model and xml reading

---
+++ InvoiceGen/Model/ObjectModel/Invoice.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceGen.Model.ObjectModel
+{
+    public class Invoice
+    {
+        public const string XmlName = "invoice";
+
+        public int id { get; set; }
+        public string title { get; set; }
+        public DateTime timestamp { get; set; }
+        public bool paid { get; set; }
+        public IList<InvoiceItem> items { get; set; }
+
+        /// <summary>
+        /// Default constructor. Initialises the collection of items.
+        /// </summary>
+        public Invoice() { this.items = new List<InvoiceItem>(); }
+
+        /// <summary>
+        /// Get total of all item amounts.
+        /// </summary>
+        /// <returns></returns>
+        public decimal getTotal()
+        {
+            if (items == null)
+                return 0;
+
+            decimal total = 0;
+
+            foreach (InvoiceItem item in this.items)
+                total += item.amount;
+
+            return total;
+        }
+    }
+}
