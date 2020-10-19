44fc1bde494b974db3e2655ce1ae394311ce722a
al-c95 <137837298+al-c95@users.noreply.github.com>
2020-10-19 22:19:12 +1100

    mask passwords

--- InvoiceGen/View/ConfigWindow.Designer.cs
+++ InvoiceGen/View/ConfigWindow.Designer.cs
@@ -29,26 +29,26 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.button_save = new System.Windows.Forms.Button();
-            this.button_cancel = new System.Windows.Forms.Button();
-            this.textBox_recipientAddress = new System.Windows.Forms.TextBox();
-            this.textBox_recipientName = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox_host = new System.Windows.Forms.TextBox();
+            this.numericUpDown_port = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox_senderEmailPassword = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox_senderName = new System.Windows.Forms.TextBox();
             this.textBox_senderAddress = new System.Windows.Forms.TextBox();
-            this.textBox_senderEmailPassword = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.numericUpDown_port = new System.Windows.Forms.NumericUpDown();
-            this.textBox_host = new System.Windows.Forms.TextBox();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
+            this.textBox_senderName = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_recipientName = new System.Windows.Forms.TextBox();
+            this.textBox_recipientAddress = new System.Windows.Forms.TextBox();
+            this.button_save = new System.Windows.Forms.Button();
+            this.button_cancel = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
-            this.groupBox2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_port)).BeginInit();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -73,6 +73,100 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Sender";
             // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(30, 175);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(29, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Port:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(30, 139);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(32, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Host:";
+            // 
+            // textBox_host
+            // 
+            this.textBox_host.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_host.Location = new System.Drawing.Point(72, 136);
+            this.textBox_host.Name = "textBox_host";
+            this.textBox_host.Size = new System.Drawing.Size(255, 20);
+            this.textBox_host.TabIndex = 11;
+            // 
+            // numericUpDown_port
+            // 
+            this.numericUpDown_port.Location = new System.Drawing.Point(72, 173);
+            this.numericUpDown_port.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDown_port.Name = "numericUpDown_port";
+            this.numericUpDown_port.Size = new System.Drawing.Size(255, 20);
+            this.numericUpDown_port.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 100);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Password:";
+            // 
+            // textBox_senderEmailPassword
+            // 
+            this.textBox_senderEmailPassword.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_senderEmailPassword.Location = new System.Drawing.Point(72, 97);
+            this.textBox_senderEmailPassword.Name = "textBox_senderEmailPassword";
+            this.textBox_senderEmailPassword.Size = new System.Drawing.Size(255, 20);
+            this.textBox_senderEmailPassword.TabIndex = 8;
+            this.textBox_senderEmailPassword.UseSystemPasswordChar = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Name:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Address:";
+            // 
+            // textBox_senderAddress
+            // 
+            this.textBox_senderAddress.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_senderAddress.Location = new System.Drawing.Point(72, 22);
+            this.textBox_senderAddress.Name = "textBox_senderAddress";
+            this.textBox_senderAddress.Size = new System.Drawing.Size(255, 20);
+            this.textBox_senderAddress.TabIndex = 4;
+            // 
+            // textBox_senderName
+            // 
+            this.textBox_senderName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_senderName.Location = new System.Drawing.Point(72, 58);
+            this.textBox_senderName.Name = "textBox_senderName";
+            this.textBox_senderName.Size = new System.Drawing.Size(255, 20);
+            this.textBox_senderName.TabIndex = 5;
+            // 
             // groupBox2
             // 
             this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
@@ -87,6 +181,42 @@
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Recipient";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Name:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Address:";
+            // 
+            // textBox_recipientName
+            // 
+            this.textBox_recipientName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_recipientName.Location = new System.Drawing.Point(72, 55);
+            this.textBox_recipientName.Name = "textBox_recipientName";
+            this.textBox_recipientName.Size = new System.Drawing.Size(255, 20);
+            this.textBox_recipientName.TabIndex = 1;
+            // 
+            // textBox_recipientAddress
+            // 
+            this.textBox_recipientAddress.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_recipientAddress.Location = new System.Drawing.Point(72, 19);
+            this.textBox_recipientAddress.Name = "textBox_recipientAddress";
+            this.textBox_recipientAddress.Size = new System.Drawing.Size(255, 20);
+            this.textBox_recipientAddress.TabIndex = 0;
             // 
             // button_save
             // 
@@ -110,135 +240,6 @@
             this.button_cancel.UseVisualStyleBackColor = true;
             this.button_cancel.Click += new System.EventHandler(this.button_cancel_Click);
             // 
-            // textBox_recipientAddress
-            // 
-            this.textBox_recipientAddress.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox_recipientAddress.Location = new System.Drawing.Point(72, 19);
-            this.textBox_recipientAddress.Name = "textBox_recipientAddress";
-            this.textBox_recipientAddress.Size = new System.Drawing.Size(255, 20);
-            this.textBox_recipientAddress.TabIndex = 0;
-            // 
-            // textBox_recipientName
-            // 
-            this.textBox_recipientName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox_recipientName.Location = new System.Drawing.Point(72, 55);
-            this.textBox_recipientName.Name = "textBox_recipientName";
-            this.textBox_recipientName.Size = new System.Drawing.Size(255, 20);
-            this.textBox_recipientName.TabIndex = 1;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(14, 22);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(48, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Address:";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(24, 58);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(38, 13);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Name:";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(24, 61);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(38, 13);
-            this.label3.TabIndex = 7;
-            this.label3.Text = "Name:";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(14, 25);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(48, 13);
-            this.label4.TabIndex = 6;
-            this.label4.Text = "Address:";
-            // 
-            // textBox_senderName
-            // 
-            this.textBox_senderName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox_senderName.Location = new System.Drawing.Point(72, 58);
-            this.textBox_senderName.Name = "textBox_senderName";
-            this.textBox_senderName.Size = new System.Drawing.Size(255, 20);
-            this.textBox_senderName.TabIndex = 5;
-            // 
-            // textBox_senderAddress
-            // 
-            this.textBox_senderAddress.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox_senderAddress.Location = new System.Drawing.Point(72, 22);
-            this.textBox_senderAddress.Name = "textBox_senderAddress";
-            this.textBox_senderAddress.Size = new System.Drawing.Size(255, 20);
-            this.textBox_senderAddress.TabIndex = 4;
-            // 
-            // textBox_senderEmailPassword
-            // 
-            this.textBox_senderEmailPassword.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox_senderEmailPassword.Location = new System.Drawing.Point(72, 97);
-            this.textBox_senderEmailPassword.Name = "textBox_senderEmailPassword";
-            this.textBox_senderEmailPassword.Size = new System.Drawing.Size(255, 20);
-            this.textBox_senderEmailPassword.TabIndex = 8;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(6, 100);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(56, 13);
-            this.label5.TabIndex = 9;
-            this.label5.Text = "Password:";
-            // 
-            // numericUpDown_port
-            // 
-            this.numericUpDown_port.Location = new System.Drawing.Point(72, 173);
-            this.numericUpDown_port.Maximum = new decimal(new int[] {
-            10000,
-            0,
-            0,
-            0});
-            this.numericUpDown_port.Name = "numericUpDown_port";
-            this.numericUpDown_port.Size = new System.Drawing.Size(255, 20);
-            this.numericUpDown_port.TabIndex = 10;
-            // 
-            // textBox_host
-            // 
-            this.textBox_host.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox_host.Location = new System.Drawing.Point(72, 136);
-            this.textBox_host.Name = "textBox_host";
-            this.textBox_host.Size = new System.Drawing.Size(255, 20);
-            this.textBox_host.TabIndex = 11;
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(30, 139);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(32, 13);
-            this.label6.TabIndex = 12;
-            this.label6.Text = "Host:";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(30, 175);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(29, 13);
-            this.label7.TabIndex = 13;
-            this.label7.Text = "Port:";
-            // 
             // ConfigWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -254,9 +255,9 @@
             this.Text = "Configuration";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_port)).EndInit();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_port)).EndInit();
             this.ResumeLayout(false);
 
         }
